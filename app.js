b54030966c4666ee9c79aa5ab8b48597d97cc6ed
FeikkoDev <you@example.com>
2020-02-28 17:04:39 +0200

    Added comments to main code

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
+
+/**
+ * FANUC ROBOT HTTP SERVER CLIENT
+ * @Auhthor Kapelajii2020
+ */
+
 const axios = require("axios");
 axios.get("https://fanuc-robot-http-server.herokuapp.com/").then(res => {
   console.log(res);
+
+  // Etsitään regexp avulla Joint   1:    -78.72 vastaava merkkijono kaikille 6 akselille
   const regexp = /Joint   \d{1}:    ([-| ]\d{1,2}.\d{1,2})/g;
   let joints = [];
+  // tallennetaan kaikki regexp vastaavat merkkijonot talteen
   let matches = res.data.matchAll(regexp);
   let count = 0;
   for (const match of matches) {
-   count++;
-   if (count > 6) break;
+    count++;
+    // koska datassa esiintyy vastaanvanlaisia merkkijonoja
+    // myöhemmin halutaan vain kuusi ensimmäistä merkkijonoa talteen
+    // nämä ovat tarvittavat akseleiden arvot
+    if (count > 6) break;
+
+    // parsitaan merkkijono desimaaliluvuksi
     const value = parseFloat(match[1]);
+    // lisätään parsittu luku taulukkoon
     joints.push(value);
 }
 
 console.log(joints)
 });
-
 
 
 /* //Luo ohjelmaan luuppi, joka pyörii ikuisesti
