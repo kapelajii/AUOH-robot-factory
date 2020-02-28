d0dca6d6ffae5905a0241f2d64df5a29ec5e0f0e
FeikkoDev <you@example.com>
2020-02-28 16:39:18 +0200

    Gets joint values  using regex

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
+const axios = require("axios");
+axios.get("https://fanuc-robot-http-server.herokuapp.com/").then(res => {
+  console.log(res);
+  const regexp = /Joint   \d{1}:    ([-| ]\d{1,2}.\d{1,2})/g;
+  let joints = [];
+  let matches = res.data.matchAll(regexp);
+  let count = 0;
+  for (const match of matches) {
+   count++;
+   if (count > 6) break;
+    const value = parseFloat(match[1]);
+    joints.push(value);
+}
+
+console.log(joints)
+});
 
 
-const axios = require('axios');
-axios.get('https://fanuc-robot-http-server.herokuapp.com/')
- .then((res) => {
- console.log(res);
- });
+
+/* //Luo ohjelmaan luuppi, joka pyörii ikuisesti
+const main_loop = () => {
+  setTimeout(() => {
+    //Koodi
+    main_loop();
+  }, 10);
+};
+main_loop();
+//Tulosta jokaisella kierroksella aikaleima, nivelarvot lista ja pyyntöön kulunut aika
+//Alla vinkkejä aikaleiman luomiseen ja viiveen laskemiseen
+const time_stamp = new Date();
+const delta = time_stamp - start_time_stamp; */
