4a2e102002ea82db3cc69dd3113dc3a68b453ba7
FeikkoDev <you@example.com>
2020-03-08 00:23:14 +0200

    fixed comments

--- app.js
+++ app.js
@@ -4,60 +4,50 @@
  * @Auhthor Kapelajii2020
  */
 
-// Otetaan MQTT-moduuli käyttöön
+// constants
 const mqtt = require('mqtt');
 const FANUC_SERVER = 'https://fanuc-robot-http-server.herokuapp.com/'
 const MQTT_SERVER = 'wss://auoh20-mqtt-broker.herokuapp.com';
 
-//Luo ohjelmaan luuppi, joka pyörii ikuisesti
+// program main-loop
 const main_loop = () => {
   setTimeout(() => {
-    // tallennetaan ohjelman aloitusaika  
+    // save program start time 
     const start_time_stamp = new Date();
-    // haetaan akseliarvot serveriltä 
+    // get robot axis values from Fanuc-server
     const axios = require("axios");
     axios.get(FANUC_SERVER).then(res => {
-    // console.log(res);
-
-        // Etsitään regexp avulla Joint   1:    -78.72 vastaava merkkijono kaikille 6 akselille
+        // regular expression to get correct data from dataset
         const regexp = /Joint   \d{1}:    ([-| ]\d{1,2}.\d{1,2})/g;
         let joints = [];
-        // tallennetaan kaikki regexp vastaavat merkkijonot talteen
         let matches = res.data.matchAll(regexp);
         let count = 0;
         for (const match of matches) {
             count++;
-            // koska datassa esiintyy vastaanvanlaisia merkkijonoja
-            // halutaan vain kuusi ensimmäistä merkkijonoa talteen
-            // nämä ovat tarvittavat akseleiden arvot
+            // get first six values from filtered dataset [axis 1-6 position]
             if (count > 6) break;
-
-            // parsitaan merkkijono desimaaliluvuksi
             const value = parseFloat(match[1]);
-            // lisätään parsittu luku taulukkoon
             joints.push(value);
             } 
-
-        // tulosta jokaisella kierroksella aikaleima, nivelarvot lista ja pyyntöön kulunut aika
-        // tallennetaan haun lopetusaika 
-        const time_stamp = new Date();
-        const delta = time_stamp - start_time_stamp; 
-        //toISOString() komennolla saadan ajan formaatti muutettu ISO-standardin mukaiseksi
-        console.log(time_stamp.toISOString()+" [ " +joints +" ] "+delta+" ms")
+        // data processing ready 
+        const time_stamp_end = new Date();
+        // calculate processing time in ms
+        const delta = time_stamp_end - start_time_stamp; 
+        console.log(time_stamp_end.toISOString()+" [ " +joints +" ] "+delta+" ms")
         
-        // data MQTT-serverille
+        // create MQTT-data 
         let data = {
           time : time_stamp.toISOString,
           joints: joints
         }
-        // julkaise data MQTT-brokerille 
+        // publish data to MQTT-broker
         mqtt_client.publish('joints',JSON.stringify(data));      
 });
     main_loop();
   }, 10);
 };
 
-// muodostetaan yhteys MQTT-brokerille
+// connect to MQTT broker
 const mqtt_client = mqtt.connect(MQTT_SERVER);
 mqtt_client.on('connect', () => {
   console.log('connected to mqtt broker');
